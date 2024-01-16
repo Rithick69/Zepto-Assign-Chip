ba79295f9c875bbd71c3ef6863e1f4d239b89162
Rithick Chowdhury <92905074+Rithick69@users.noreply.github.com>
2024-01-17 00:24:30 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const data = [
+  {
+    id: 0,
+    name: "Rock",
+    email: "rocka@dsse.com",
+    active: "",
+  },
+  {
+    id: 1,
+    name: "Mick",
+    email: "micka@dsse.com",
+    active: "",
+  },
+  {
+    id: 2,
+    name: "Nick",
+    email: "nicka@dsse.com",
+    active: "",
+  },
+  {
+    id: 3,
+    name: "Ross",
+    email: "rossa@dsse.com",
+    active: "",
+  },
+  {
+    id: 4,
+    name: "David",
+    email: "davida@dsse.com",
+    active: "",
+  },
+  {
+    id: 5,
+    name: "Joey",
+    email: "joeya@dsse.com",
+    active: "",
+  },
+  {
+    id: 6,
+    name: "monty",
+    email: "monty@dsse.com",
+    active: "",
+  },
+  {
+    id: 7,
+    name: "Rick",
+    email: "ricka@dsse.com",
+    active: "",
+  },
+  {
+    id: 8,
+    name: "Ram",
+    email: "ram@dsse.com",
+    active: "",
+  },
+  {
+    id: 9,
+    name: "Blake",
+    email: "blakea@dsse.com",
+    active: "",
+  },
+];
